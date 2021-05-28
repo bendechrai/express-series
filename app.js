75eb184bfb657fd9281ed402a54c25f0dfa28759
Ben Dechrai <ben@dechrai.com>
2021-05-28 12:16:54 +1000

    Added OpenID Connect capabilities Added nodemon for app auto-reloading

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var indexRouter = require("./routes/index.js");
+const { auth } = require("express-openid-connect");
+require("dotenv").config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
 
 var app = express();
 app.set("views", "views");
@@ -7,6 +18,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(auth(config));
 
 app.use("/", indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express Demo" });
+  console.log(req.oidc.isAuthenticated());
+  res.render("index", {
+    title: "Express Demo",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+  });
 });
 
 module.exports = router;
