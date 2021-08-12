4b703896f3d777838745dab728c12ac864c96633
Ben Dechrai <ben@dechrai.com>
2021-08-12 16:00:47 +1000

    Removed debug log

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log(req.oidc.isAuthenticated());
   res.render("index", {
     title: "Express Demo",
     isAuthenticated: req.oidc.isAuthenticated(),
